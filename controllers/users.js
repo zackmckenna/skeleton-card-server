3a6a225ae2e08a53fa5cbeb15948e470291a9db9
Zack McKenna <zackmckenna@gmail.com>
2020-02-27 13:59:58 -0500

    added lintconfig

--- controllers/users.js
+++ controllers/users.js
@@ -1,56 +1,56 @@
-const bcrypt = require('bcrypt');
-const usersRouter = require('express').Router();
-const User = require('../models/user');
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User
-    .find({}).populate('notes',{ content: 1, date: 1 });
-  response.json(users.map(user => user.toJSON()));
-});
+    .find({}).populate('notes',{ content: 1, date: 1 })
+  response.json(users.map(user => user.toJSON()))
+})
 
 usersRouter.delete('/:id', async (request, response, next) => {
   try {
-    await User.findByIdAndRemove(request.params.id);
-    response.status(204).end();
+    await User.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   } catch (exception) {
-    next(exception);
+    next(exception)
   }
-});
+})
 
 usersRouter.get('/:id', async (request, response, next) => {
   try{
-    const user = await User.findById(request.params.id);
+    const user = await User.findById(request.params.id)
     if (user) {
-      response.json(user.toJSON());
+      response.json(user.toJSON())
     } else {
-      response.status(404).end();
+      response.status(404).end()
     }
   } catch(exception) {
-    next(exception);
+    next(exception)
   }
-});
+})
 
 usersRouter.post('/', async (request, response, next) => {
   try {
-    console.log(request.body);
-    const body = request.body;
+    console.log(request.body)
+    const body = request.body
 
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
     const user = new User({
       username: body.username,
       name: body.name,
       email: body.email,
       passwordHash,
-    });
+    })
 
-    const savedUser = await user.save();
+    const savedUser = await user.save()
 
-    response.json(savedUser);
+    response.json(savedUser)
   } catch (exception) {
-    next(exception);
+    next(exception)
   }
-});
+})
 
-module.exports = usersRouter;
+module.exports = usersRouter
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -12,26 +12,25 @@ const userSchema = new mongoose.Schema({
     type: String
   },
   passwordHash: String,
-  email: String,
   notes: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Note'
     }
   ]
-});
+})
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator)
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-    delete returnedObject.passwordHash;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
   }
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
