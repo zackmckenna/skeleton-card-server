a80d7a7faf87db1591ac864c3572f0b7189b01b0
Zack McKenna <zackmckenna@gmail.com>
2020-03-02 16:08:45 -0500

    updates to socket.io server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
+const socketIo = require('socket.io')
 
 const server = http.createServer(app)
+const io = socketIo(server)
+
+io.on('connection', function(socket) {
+  console.log('user connected', socket.id)
+  io.emit('client connected', socket.id)
+})
 
 const PORT = process.env.PORT || 3003
 
